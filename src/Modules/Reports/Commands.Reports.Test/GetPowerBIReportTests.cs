bd261f3ae4291e6475a53c509cb519ca4d446800
Chaz Beck <chbeck@microsoft.com>
2018-02-26 12:23:15 -0800

    Added Commands.Reports.Test project

---
+++ src/Modules/Reports/Commands.Reports.Test/GetPowerBIReportTests.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Management.Automation;
+using Microsoft.PowerBI.Commands.Profile;
+using Microsoft.PowerBI.Commands.Reports;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Commands.Reports.Test
+{
+    [TestClass]
+    public class GetPowerBIReportTests
+    {
+        [TestMethod]
+        [TestCategory("Interactive")]
+        [TestCategory("SkipWhenLiveUnitTesting")] // Ignore for Live Unit Testing
+        public void EndToEndGetReports()
+        {
+            using (var ps = System.Management.Automation.PowerShell.Create())
+            {
+                ps.AddCommand(new CmdletInfo($"{ConnectPowerBIServiceAccount.CmdletVerb}-{ConnectPowerBIServiceAccount.CmdletName}", typeof(ConnectPowerBIServiceAccount)));
+                var result = ps.Invoke();
+                Assert.IsNotNull(result);
+                ps.Commands.Clear();
+                ps.AddCommand(new CmdletInfo($"{GetPowerBIReport.CmdletVerb}-{GetPowerBIReport.CmdletName}", typeof(GetPowerBIReport)));
+                result = ps.Invoke();
+                Assert.IsNotNull(result);
+                Assert.IsTrue(result.Count > 0);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CmdletInvocationException))]
+        public void CallGetReportsWithoutLogin()
+        {
+            using (var ps = System.Management.Automation.PowerShell.Create())
+            {
+                ps.AddCommand(new CmdletInfo($"{GetPowerBIReport.CmdletVerb}-{GetPowerBIReport.CmdletName}", typeof(GetPowerBIReport)));
+                var result = ps.Invoke();
+                Assert.Fail("Should not have reached this point");
+            }
+        }
+    }
+}
