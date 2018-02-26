175fad78f87c1548d57c47bff420e672e1523238
Chaz Beck <chbeck@microsoft.com>
2018-02-26 12:21:11 -0800

    Added Commands.Workspaces.Test project

---
+++ src/Modules/Workspaces/Commands.Workspaces.Test/GetPowerBIWorkspaceTests.cs
@@ -0,0 +1,41 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Management.Automation;
+using Microsoft.PowerBI.Commands.Profile;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.PowerBI.Commands.Workspaces.Test
+{
+    [TestClass]
+    public class GetPowerBIWorkspaceTests
+    {
+        [TestMethod]
+        [TestCategory("Interactive")]
+        [TestCategory("SkipWhenLiveUnitTesting")] // Ignore for Live Unit Testing
+        public void EndToEndGetWorkspaces()
+        {
+            using (var ps = System.Management.Automation.PowerShell.Create())
+            {
+                ps.AddCommand(new CmdletInfo($"{ConnectPowerBIServiceAccount.CmdletVerb}-{ConnectPowerBIServiceAccount.CmdletName}", typeof(ConnectPowerBIServiceAccount)));
+                var result = ps.Invoke();
+                Assert.IsNotNull(result);
+                ps.Commands.Clear();
+                ps.AddCommand(new CmdletInfo($"{GetPowerBIWorkspace.CmdletVerb}-{GetPowerBIWorkspace.CmdletName}", typeof(GetPowerBIWorkspace)));
+                result = ps.Invoke();
+                Assert.IsNotNull(result);
+                Assert.IsTrue(result.Count > 0);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CmdletInvocationException))]
+        public void CallGetWorkspacesWithoutLogin()
+        {
+            using (var ps = System.Management.Automation.PowerShell.Create())
+            {
+                ps.AddCommand(new CmdletInfo($"{GetPowerBIWorkspace.CmdletVerb}-{GetPowerBIWorkspace.CmdletName}", typeof(GetPowerBIWorkspace)));
+                var result = ps.Invoke();
+                Assert.Fail("Should not have reached this point");
+            }
+        }
+    }
+}
