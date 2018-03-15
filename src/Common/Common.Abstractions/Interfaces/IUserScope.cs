d05b034ed75aadbd04aac52de5c209ab0699e171
Chaz Beck <chbeck@microsoft.com>
2018-03-15 14:08:05 -0700

    Added GetWorkspacesAsAdmin call (using private package)

---
+++ src/Common/Common.Abstractions/Interfaces/IUserScope.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.PowerBI.Common.Abstractions.Interfaces
+{
+    public interface IUserScope
+    {
+        PowerBIUserScope Scope { get; set; }
+    }
+}
--- src/Modules/Workspaces/Commands.Workspaces/GetPowerBIWorkspace.cs
+++ src/Modules/Workspaces/Commands.Workspaces/GetPowerBIWorkspace.cs
@@ -8,23 +8,31 @@ using System.Management.Automation;
 using Microsoft.PowerBI.Api.V2;
 using Microsoft.PowerBI.Api.V2.Models;
 using Microsoft.PowerBI.Commands.Common;
+using Microsoft.PowerBI.Common.Abstractions;
+using Microsoft.PowerBI.Common.Abstractions.Interfaces;
 using Microsoft.Rest;
 
 namespace Microsoft.PowerBI.Commands.Workspaces
 {
     [Cmdlet(CmdletVerb, CmdletName)]
     [OutputType(typeof(IEnumerable<Group>))]
-    public class GetPowerBIWorkspace : PowerBICmdlet
+    public class GetPowerBIWorkspace : PowerBICmdlet, IUserScope
     {
         public const string CmdletName = "PowerBIWorkspace";
         public const string CmdletVerb = VerbsCommon.Get;
+
+        #region Parameters
+
+        public PowerBIUserScope Scope { get; set; } = PowerBIUserScope.Individual;
+
+        #endregion
 
         protected override void ExecuteCmdlet()
         {
             var token = this.Authenticator.Authenticate(this.Profile, this.Logger, this.Settings);
             var client = new PowerBIClient(new TokenCredentials(token.AccessToken));
 
-            var workspaces = client.Groups.GetGroups();
+            var workspaces = this.Scope.Equals(PowerBIUserScope.Individual) ? client.Groups.GetGroups() : client.Groups.GetGroupsAsAdmin();
             this.Logger.WriteObject(workspaces.Value, true);
         }
     }
