369ee5c3b15f74f6bbae177477289c2d76d1d6a5
nandemoii <nandemoii@users.noreply.github.com>
2018-03-21 15:56:30 -0700

    Add Set-PowerBIWorkspace cmdlet (#3)
    * Add Set-PowerBIWorkspace cmdlet, unit tests, and test utilities
    
    * Update readme with description of Set-PowerBIWorkspace cmdlet
    
    * Address pull request feedback
    
    * Update script parameter type to be hashMap to resolve local run issue
    
    * Upate documentation with information for Set-PowerBIWorkspace
    
    * Add test for using both parameter sets

---
+++ src/Modules/Workspaces/Commands.Workspaces.Test/SetPowerBIWorkspaceTests.cs
@@ -0,0 +1,173 @@
+﻿/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using Commands.Common.Test;
+using Microsoft.PowerBI.Api.V2.Models;
+using Microsoft.PowerBI.Commands.Profile.Test;
+using Microsoft.PowerBI.Common.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.PowerBI.Commands.Workspaces.Test
+{
+    [TestClass]
+    public class SetPowerBIWorkspaceTests
+    {
+        private static CmdletInfo Cmdlet => new CmdletInfo($"{SetPowerBIWorkspace.CmdletVerb}-{SetPowerBIWorkspace.CmdletName}", typeof(SetPowerBIWorkspace));
+
+        [TestMethod]
+        [TestCategory("Interactive")]
+        [TestCategory("SkipWhenLiveUnitTesting")] // Ignore for Live Unit Testing
+        public void EndToEndSetWorkspaceOrganizationScopePropertiesParameterSet()
+        {
+            using (var ps = System.Management.Automation.PowerShell.Create())
+            {
+                ProfileTestUtilities.ConnectToPowerBI(ps);
+
+                var workspace = WorkspacesTestUtilities.GetWorkspace(ps);
+                if (workspace == null)
+                {
+                    Assert.Inconclusive("No workspaces found to perform end to end test");
+                }
+
+                var updatedName = TestUtilities.GetRandomString();
+                var updatedDescription = TestUtilities.GetRandomString();
+                var parameters = new Dictionary<string, object>
+                {
+                    { "Scope", PowerBIUserScope.Organization },
+                    { "Id", workspace.Id },
+                    { "Name", updatedName },
+                    { "Description", updatedDescription },
+                };
+                ps.AddCommand(Cmdlet).AddParameters(parameters);
+
+                var result = ps.Invoke();
+                ps.Commands.Clear();
+
+                Assert.IsNotNull(result);
+                var updatedWorkspace = WorkspacesTestUtilities.GetWorkspace(ps, workspace.Id);
+                Assert.AreEqual(updatedName, updatedWorkspace.Name);
+                Assert.AreEqual(updatedDescription, updatedWorkspace.Description);
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Interactive")]
+        [TestCategory("SkipWhenLiveUnitTesting")] // Ignore for Live Unit Testing
+        public void EndToEndSetWorkspaceOrganizationScopeWorkspaceParameterSet()
+        {
+            using (var ps = System.Management.Automation.PowerShell.Create())
+            {
+                ProfileTestUtilities.ConnectToPowerBI(ps);
+
+                var workspace = WorkspacesTestUtilities.GetWorkspace(ps);
+                if (workspace == null)
+                {
+                    Assert.Inconclusive("No workspaces found to perform end to end test");
+                }
+
+                var updatedName = TestUtilities.GetRandomString();
+                var updatedDescription = TestUtilities.GetRandomString();
+                workspace.Name = updatedName;
+                workspace.Description = updatedDescription;
+                var parameters = new Dictionary<string, object>
+                {
+                    { "Scope", PowerBIUserScope.Organization },
+                    { "Workspace", workspace },
+                };
+                ps.AddCommand(Cmdlet).AddParameters(parameters);
+
+                var result = ps.Invoke();
+                ps.Commands.Clear();
+
+                Assert.IsNotNull(result);
+                var updatedWorkspace = WorkspacesTestUtilities.GetWorkspace(ps, workspace.Id);
+                Assert.AreEqual(updatedName, updatedWorkspace.Name);
+                Assert.AreEqual(updatedDescription, updatedWorkspace.Description);
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Interactive")]
+        [TestCategory("SkipWhenLiveUnitTesting")] // Ignore for Live Unit Testing
+        public void EndToEndSetWorkspaceIndividualScope()
+        {
+            using (var ps = System.Management.Automation.PowerShell.Create())
+            {
+                ProfileTestUtilities.ConnectToPowerBI(ps);
+                var parameters = new Dictionary<string, object>
+                {
+                    { "Scope", PowerBIUserScope.Individual },
+                    { "Id", new Guid() },
+                    { "Name", "Updated Workspace Name" },
+                    { "Description", "Updated Workspace Description" },
+                };
+                ps.AddCommand(Cmdlet).AddParameters(parameters);
+
+                try
+                {
+                    ps.Invoke();
+
+                    Assert.Fail("Should not have reached this point");
+                }
+                catch (CmdletInvocationException ex)
+                {
+                    Assert.AreEqual(ex.InnerException.GetType(), typeof(NotImplementedException));
+                }
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CmdletInvocationException))]
+        public void CallSetWorkspaceWithoutLogin()
+        {
+            using (var ps = System.Management.Automation.PowerShell.Create())
+            {
+                var parameters = new Dictionary<string, object> { { "Id", new Guid() } };
+                ps.AddCommand(Cmdlet).AddParameters(parameters);
+
+                ps.Invoke();
+
+                Assert.Fail("Should not have reached this point");
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ParameterBindingException))]
+        public void CallSetWorkspaceWithoutRequiredParameterIdOrWorkspace()
+        {
+            using (var ps = System.Management.Automation.PowerShell.Create())
+            {
+                ps.AddCommand(Cmdlet);
+
+                var result = ps.Invoke();
+
+                Assert.Fail("Should not have reached this point");
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ParameterBindingException))]
+        public void CallSetWorkspaceWithBothParameterSets()
+        {
+            using (var ps = System.Management.Automation.PowerShell.Create())
+            {
+                var parameters = new Dictionary<string, object>
+                {
+                    { "Scope", PowerBIUserScope.Organization },
+                    { "Id", new Guid() },
+                    { "Workspace", new Group() }
+                };
+                ps.AddCommand(Cmdlet).AddParameters(parameters);
+
+                var result = ps.Invoke();
+
+                Assert.Fail("Should not have reached this point");
+            }
+        }
+    }
+}
---
+++ src/Modules/Workspaces/Commands.Workspaces/SetPowerBIWorkspace.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+using System;
+using System.Management.Automation;
+using Microsoft.PowerBI.Api.V2;
+using Microsoft.PowerBI.Api.V2.Models;
+using Microsoft.PowerBI.Commands.Common;
+using Microsoft.PowerBI.Common.Abstractions;
+using Microsoft.PowerBI.Common.Abstractions.Interfaces;
+using Microsoft.Rest;
+
+namespace Microsoft.PowerBI.Commands.Workspaces
+{
+    [Cmdlet(CmdletVerb, CmdletName, DefaultParameterSetName = PropertiesParameterSetName)]
+    public class SetPowerBIWorkspace : PowerBICmdlet, IUserScope
+    {
+        public const string CmdletName = "PowerBIWorkspace";
+        public const string CmdletVerb = VerbsCommon.Set;
+
+        private const string PropertiesParameterSetName = "Properties";
+        private const string WorkspaceParameterSetName = "Workspace";
+
+        #region Parameters
+
+        [Parameter(Mandatory = false, ParameterSetName = PropertiesParameterSetName)]
+        [Parameter(Mandatory = false, ParameterSetName = WorkspaceParameterSetName)]
+        public PowerBIUserScope Scope { get; set; } = PowerBIUserScope.Individual;
+
+        [Parameter(Mandatory = true, ParameterSetName = PropertiesParameterSetName)]
+        public Guid Id { get; set; }
+
+        [Parameter(Mandatory = false, ParameterSetName = PropertiesParameterSetName)]
+        public string Name { get; set; }
+
+        [Parameter(Mandatory = false, ParameterSetName = PropertiesParameterSetName)]
+        public string Description { get; set; }
+
+        [Parameter(Mandatory = true, ParameterSetName = WorkspaceParameterSetName)]
+        public Group Workspace { get; set; }
+
+        #endregion
+
+        protected override void ExecuteCmdlet()
+        {
+            if (this.Scope.Equals(PowerBIUserScope.Individual))
+            {
+                throw new NotImplementedException();
+            }
+
+            PowerBIClient client = null;
+            var token = this.Authenticator.Authenticate(this.Profile, this.Logger, this.Settings);
+            if (Uri.TryCreate(this.Profile.Environment.GlobalServiceEndpoint, UriKind.Absolute, out Uri baseUri))
+            {
+                client = new PowerBIClient(baseUri, new TokenCredentials(token.AccessToken));
+            }
+            else
+            {
+                client = new PowerBIClient(new TokenCredentials(token.AccessToken));
+            }
+
+            if (this.ParameterSetName.Equals(PropertiesParameterSetName))
+            {
+                var updatedGroup = new Group { Name = this.Name, Description = this.Description };
+                var response = client.Groups.UpdateGroupAsAdmin(this.Id.ToString(), updatedGroup);
+                this.Logger.WriteObject(response);
+            }
+            else if (this.ParameterSetName.Equals(WorkspaceParameterSetName))
+            {
+                var response = client.Groups.UpdateGroupAsAdmin(this.Workspace.Id.ToString(), this.Workspace);
+                this.Logger.WriteObject(response);
+            }
+        }
+    }
+}
