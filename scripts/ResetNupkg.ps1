494389c93532f70e1379b7c3fd0266a84d174b34
Chaz Beck <chbeck@microsoft.com>
2018-03-28 14:22:04 -0700

    Script to reset Nuget packages for being repacked; Created Common.Client project

---
+++ scripts/ResetNupkg.ps1
@@ -0,0 +1,55 @@
+[CmdletBinding()]
+param
+(
+    [ValidateNotNullOrEmpty()]
+    [string] $Path,
+
+    [ValidateNotNullOrEmpty()]
+    [string] $Destination,
+
+    [ValidateNotnull()]
+    [string[]] $ItemsToDelete = @('_rels', 'package', '[Content_Types].xml')
+)
+
+Write-Output "Running $($MyInvocation.MyCommand.Name)"
+if($Path -eq $Destination) {
+    throw "-Path and -Destination have to be different"
+}
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+[System.IO.FileInfo[]]$nupkgFiles = Get-ChildItem -Path $Path -File -Filter '*.nupkg'
+if(!$nupkgFiles) {
+    throw "Found no NUPKG files in: $Path"
+}
+
+Write-Output "Nuget Packages:`n$($nupkgFiles | Select-Object FullName | Out-String)"
+
+[void](Remove-item -Path $Destination -Force -Recurse -ErrorAction SilentlyContinue)
+[void](New-Item -Path $Destination -ItemType Directory -Force -ErrorAction SilentlyContinue)
+foreach($nupkgFile in $nupkgFiles) {
+    Write-Output "Extracting $($nupkgFile.FullName)..."
+    $nupkgDestination = Join-Path -Path $Destination -ChildPath $nupkgFile.BaseName
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($nupkgFile.FullName, $nupkgDestination)
+}
+
+Write-Output "Looking for nuspec files under $Destination"
+[System.IO.DirectoryInfo[]]$nuspecDirectories = Get-ChildItem -Recurse -Path $Destination -Filter '*.nuspec' | ForEach-Object { $_.Directory }
+foreach($directory in $nuspecDirectories) {
+    Get-ChildItem -Path $directory.FullName | Where-Object { $_.Name -in $ItemsToDelete } | ForEach-Object {
+        Write-Output "Deleting $($_.FullName)"
+        $_ | Remove-Item -Force -ErrorAction Continue -Recurse
+    }
+
+    $libFolder = Join-Path -Path $directory.FullName -ChildPath 'lib'
+    if(Test-Path -Path $libFolder) {
+        Write-Output "Lib founder found: $libFolder"
+        Get-ChildItem -Path $libFolder -Directory | Where-Object { $_.BaseName -like '*%2B*'} | ForEach-Object {
+            $newName = $_.BaseName -replace '%2B', '+'
+            Write-Output "Renaming '$($_.BaseName)' to '$newName'"
+            Rename-Item -Path $_.FullName -NewName $newName
+        }
+    }
+}
+
+Write-Output "Finished running $($MyInvocation.MyCommand.Name)"
---
+++ src/Common/Common.Client/PowerBIClientCmdlet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.PowerBI.Api.V2;
+using Microsoft.PowerBI.Commands.Common;
+using Microsoft.PowerBI.Common.Abstractions.Interfaces;
+using Microsoft.Rest;
+
+namespace Microsoft.PowerBI.Common.Client
+{
+    public abstract class PowerBIClientCmdlet : PowerBICmdlet
+    {
+        public PowerBIClientCmdlet() : base()
+        {
+        }
+
+        public PowerBIClientCmdlet(IPowerBICmdletInitFactory init) : base(init)
+        {
+        }
+
+        protected IPowerBIClient CreateClient()
+        {
+            var token = this.Authenticator.Authenticate(this.Profile, this.Logger, this.Settings);
+            if (Uri.TryCreate(this.Profile.Environment.GlobalServiceEndpoint, UriKind.Absolute, out Uri baseUri))
+            {
+                return new PowerBIClient(baseUri, new TokenCredentials(token.AccessToken));
+            }
+            else
+            {
+                return new PowerBIClient(new TokenCredentials(token.AccessToken));
+            }
+        }
+    }
+}
