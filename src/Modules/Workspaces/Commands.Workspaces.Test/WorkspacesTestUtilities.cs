a8510b195fd6256bf4a38045f488f83d1e5c2add
nandemoii <nandemoii@users.noreply.github.com>
2018-04-13 12:46:40 -0700

    Update cmdlets and tests to handle legacy workspaces properly (#12)
    * Correct issues with end-to-end tests that caused consistent failures
    
    * Update GetPowerBIWorkspace cmdlet to send query options to the SDK for non-admin scenario
    
    * Add explanatory comment regarding the orphaned filter and legacy workspaces
    
    * Add cmdlet error checking to tests based on review feedback

--- src/Modules/Workspaces/Commands.Workspaces.Test/WorkspacesTestUtilities.cs
+++ src/Modules/Workspaces/Commands.Workspaces.Test/WorkspacesTestUtilities.cs
@@ -3,33 +3,96 @@
  * Licensed under the MIT License.
  */
 
+using System.Collections.Generic;
 using System.Linq;
 using System.Management.Automation;
 using Commands.Common.Test;
 using Microsoft.PowerBI.Api.V2.Models;
 using Microsoft.PowerBI.Common.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Microsoft.PowerBI.Commands.Workspaces.Test
 {
     public static class WorkspacesTestUtilities
     {
-        public static CmdletInfo GetPowerBIWorkspaceCmdletInfo { get; } = new CmdletInfo($"{GetPowerBIWorkspace.CmdletVerb}-{GetPowerBIWorkspace.CmdletName}", typeof(GetPowerBIWorkspace));
+        public static CmdletInfo GetPowerBIWorkspaceCmdletInfo => new CmdletInfo($"{GetPowerBIWorkspace.CmdletVerb}-{GetPowerBIWorkspace.CmdletName}", typeof(GetPowerBIWorkspace));
 
-        public static Group GetWorkspace(System.Management.Automation.PowerShell ps, string id = null, PowerBIUserScope scope = PowerBIUserScope.Individual)
+        public static void AssertShouldContinueOrganizationTest(Group workspace)
         {
-            ps.AddCommand(GetPowerBIWorkspaceCmdletInfo).AddParameter(nameof(GetPowerBIWorkspace.Scope), scope.ToString());
-            var results = ps.Invoke();
-            TestUtilities.AssertNoCmdletErrors(ps);
-            ps.Commands.Clear();
+            if (workspace == null)
+            {
+                Assert.Inconclusive("No workspaces returned. Verify you have workspaces in your organization.");
+            }
+        }
 
+        public static void AssertShouldContinueIndividualTest(Group workspace)
+        {
+            if (workspace == null)
+            {
+                Assert.Inconclusive("No workspaces returned. Verify you are assigned or own any workspaces.");
+            }
+        }
+
+        public static Group GetWorkspace(System.Management.Automation.PowerShell ps, PowerBIUserScope scope, string id)
+        {
+            var results = InvokeGetPowerBIWorkspace(ps, scope);
             if (results.Any())
             {
                 var workspaces = results.Select(x => (Group)x.BaseObject);
-
-                return id == null ? workspaces.First() : workspaces.First(x => x.Id == id);
+                return workspaces.First(x => x.Id == id);
             }
 
             return null;
+        }
+
+        public static Group GetFirstWorkspace(System.Management.Automation.PowerShell ps, PowerBIUserScope scope)
+        {
+            var results = InvokeGetPowerBIWorkspace(ps, scope);
+            if (results.Any())
+            {
+                var workspaces = results.Select(x => (Group)x.BaseObject);
+                return workspaces.First();
+            }
+
+            return null;
+        }
+
+        // TODO: Until the non-admin endpoint exposes type, this can only call the cmdlet with Organization scope
+        public static Group GetFirstWorkspaceInOrganization(System.Management.Automation.PowerShell ps)
+        {
+            var results = InvokeGetPowerBIWorkspace(ps, PowerBIUserScope.Organization);
+            if (results.Any())
+            {
+                var workspaces = results.Select(x => (Group)x.BaseObject);
+                return workspaces.FirstOrDefault(x => x.Type == WorkspaceType.Workspace);
+            }
+
+            return null;
+        }
+
+        // TODO: Until the non-admin endpoint supports users, this can only call the cmdlet with Organization scope
+        public static Group GetFirstWorkspaceWithUsersInOrganization(System.Management.Automation.PowerShell ps)
+        {
+            var results = InvokeGetPowerBIWorkspace(ps, PowerBIUserScope.Organization);
+            if (results.Any())
+            {
+                var workspaces = results.Select(x => (Group)x.BaseObject);
+                return workspaces.FirstOrDefault(x => x.Type == WorkspaceType.Workspace && x.Users.Any());
+            }
+
+            return null;
+        }
+
+        private static ICollection<PSObject> InvokeGetPowerBIWorkspace(System.Management.Automation.PowerShell ps, PowerBIUserScope scope)
+        {
+            ps.Commands.Clear();
+            ps.AddCommand(GetPowerBIWorkspaceCmdletInfo).AddParameter(nameof(GetPowerBIWorkspace.Scope), scope.ToString());
+
+            var results = ps.Invoke();
+
+            TestUtilities.AssertNoCmdletErrors(ps);
+            ps.Commands.Clear();
+            return results;
         }
     }
 }
