71c18f711c22667f8cb2fc9c76683416f26f9498
Chaz Beck <CodeCyclone@users.noreply.github.com>
2018-04-16 13:24:49 -0700

    Feature/updatesformetamodule (#13)
    * Added the Restore cmdlet, tests and help.
    Updated the UpdateModuleDependencies script based on feedback.
    Added the script to download nupkg.
    
    * Added the Restore cmdlet, tests and help.
    Updated the UpdateModuleDependencies script based on feedback.
    Added the script to download nupkg.
    
    * Fixes to DownloadNupkg.ps1 and UpdateModuleDependencies.ps1
    
    * Updates to DownloadNupkg.ps1
    
    * Iterating over package names in DownloadNupkg.ps1
    
    * Updates based on Release run
    
    * Fixes to scripts and fix to CallRestoreWorkspaceWithoutLogin test
    
    * Fixes to appveyor.yml
    
    * Adding logger to appveyor, adding back configuration
    
    * Harding Release for now in appveyor.yml
    
    * Adding test execution into appveyor
    
    * Referencing just the metamodule in DownloadNupkg.ps1
    
    * Correct compile-time issue after merge
    
    * Fix to GenerateFileCatalog to make CAT file have proper casing in name
    
    * Adding doc to scripts
    
    * Remove Restore-PowerBIWorkspace cmdlet changes from branch
    
    * Update readme file
    
    * Fixes from PR; Added doc to UpdatePSModuleManifest.ps1

---
+++ scripts/DownloadNupkg.ps1
@@ -0,0 +1,91 @@
+##############################
+#.SYNOPSIS
+# Downloads *.nupkg files from AppVeyor.
+#
+#.DESCRIPTION
+# Downloads and unpacks *.nupkg files on AppVeyor from powerbi-powershell builds.
+#
+#.EXAMPLE
+# PS:> .\DownloadNupkg.ps1 -OutputDirectory ..\Out
+# Downloads the latest package on AppVeyor to ..\Out directory.
+#
+#.NOTES
+# Uses the nuget.exe, must be discoverable in $env:Path.
+##############################
+[CmdletBinding()]
+param
+(
+    # Output directory of modules.
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string] $OutputDirectory,
+
+    # NuGet feed source. Default is 'https://ci.appveyor.com/nuget/powerbi-powershell-j0f50attoqd6'.
+    [ValidateNotNullOrEmpty()]
+    [string] $NugetSource = "https://ci.appveyor.com/nuget/powerbi-powershell-j0f50attoqd6",
+
+    # Nuget packages to pull. Package dependencies will also get pulled if using a newer version of NuGet.exe. If null, it will be discovered from NugetSource. Default is 'MicrosoftPowerBIMgmt'.
+    [string[]] $PackageNames = @('MicrosoftPowerBIMgmt'),
+
+    # Package version to use. If null, latest version is used.
+    [string] $PackageVersion,
+
+    # Indicates to use Prerelease package version.
+    [switch] $Prerelease
+)
+
+Write-Output "Running $($MyInvocation.MyCommand.Name)"
+$nugetExe = Get-Command 'nuget.exe' -ErrorAction Stop
+
+if(!(Test-Path -Path  $OutputDirectory)) {
+    [void](New-Item -Path $OutputDirectory -ItemType Directory -Force)
+}
+
+if(!$PackageNames -or $PackageNames.Count -lt 1) {
+    Write-Output "Determining package names..."
+    $nugetArgs = @('list', '-Source', $NugetSource, '-ForceEnglishOutput', '-NonInteractive')
+    if($PackageVersion) {
+        $nugetArgs += '-AllVersions'
+    }
+
+    if($Prerelease) {
+        $nugetArgs += '-Prerelease'
+    }
+
+    Write-Verbose "Running: & $nugetExe $($nugetArgs -join ' ')"
+    $nugetPackages = & $nugetExe $nugetArgs
+    if($LASTEXITCODE -ne 0) {
+        throw "Nuget.exe failed with exit code: $LASTEXITCODE"
+    }
+
+    $groupedPackgesByVersion = $nugetPackages | ForEach-Object { $t = $_ -split ' '; [pscustomobject]@{Name=$t[0]; Version=$t[1] }} | Group-Object -Property Version -AsHashTable
+    if($PackageVersion) {
+        $PackageNames = ($groupedPackgesByVersion[$PackageVersion]).Name
+    }
+    else {
+        $PackageNames = ($groupedPackgesByVersion.Values).Name
+    }
+}
+
+Write-Output "Installing the nuget package(s) '$($PackageNames -join ', ')' from source '$NugetSource' to output directory '$OutputDirectory'"
+
+foreach($packageName in $PackageNames) {
+    Write-Output "Getting package: $packageName"
+    $nugetArgs = @('install', $packageName, '-OutputDirectory', $OutputDirectory, '-Source', $NugetSource, '-PackageSaveMode', 'nuspec', '-NonInteractive')
+    
+    if($PackageVersion) {
+        $nugetArgs += @('-Version', $PackageVersion)
+    }
+    
+    if($Prerelease) {
+        $nugetArgs += '-Prerelease'
+    }
+
+    Write-Verbose "Running: & $nugetExe $($nugetArgs -join ' ')"
+    & $nugetExe $nugetArgs
+    if($LASTEXITCODE -ne 0) {
+        throw "Nuget.exe failed with exit code: $LASTEXITCODE"
+    }
+}
+
+Write-Output "Completed running $($MyInvocation.MyCommand.Name)"
