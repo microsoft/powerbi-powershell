2fbc99b4c3ea54f0f0e11d9a7ff1c9b9db63b0ed
Kenichiro Nakamura <kenakamu@microsoft.com>
2018-08-18 02:18:28 +0900

    remove and sort using

--- src/Modules/Data/Commands.Data/RemovePowerBIRows.cs
+++ src/Modules/Data/Commands.Data/RemovePowerBIRows.cs
@@ -4,16 +4,10 @@
  */
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Management.Automation;
-using System.Text;
-using Microsoft.PowerBI.Common.Abstractions;
-using Microsoft.PowerBI.Common.Abstractions.Interfaces;
 using Microsoft.PowerBI.Common.Api.Datasets;
 using Microsoft.PowerBI.Common.Api.Workspaces;
 using Microsoft.PowerBI.Common.Client;
-using Newtonsoft.Json.Linq;
 
 namespace Microsoft.PowerBI.Commands.Data
 {
