13cb0c63d8591b534c29b14b2ead785144a60d05
Chaz Beck <chbeck@microsoft.com>
2018-11-26 16:13:55 -0800

    Adding GCC High and GCC Military to environment list

--- src/Common/Common.Abstractions/PowerBIEnvironmentType.cs
+++ src/Common/Common.Abstractions/PowerBIEnvironmentType.cs
@@ -15,6 +15,8 @@ namespace Microsoft.PowerBI.Common.Abstractions
         Public = 0,
         Germany = 1,
         USGov = 2,
-        China = 3
+        China = 3,
+        USGovHigh = 4,
+        USGovMil = 5
     }
 }
