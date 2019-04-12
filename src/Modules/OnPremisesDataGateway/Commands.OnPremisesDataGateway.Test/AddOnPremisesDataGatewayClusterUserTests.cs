c7d2802b8a3e61cbb4a58c78e1b74f2ee2a56298
Jeff Tyson <jetyson@microsoft.com>
2019-04-12 22:25:45 +0000

    AddOnPremisesDataGatewayClusterUser cmdlet + test

---
+++ src/Modules/OnPremisesDataGateway/Commands.OnPremisesDataGateway.Test/AddOnPremisesDataGatewayClusterUserTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Management.Automation;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.PowerBI.Commands.Common.Test;
+using Microsoft.PowerBI.Commands.Profile.Test;
+using Microsoft.PowerBI.Common.Api.Gateways.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.PowerBI.Commands.OnPremisesDataGateway.Test
+{
+    [TestClass]
+    public class AddOnPremisesDataGatewayClusterUserTests
+    {
+        public static CmdletInfo AddOnPremisesDataGatewayClusterUserInfo { get; } = new CmdletInfo(
+            $"{AddOnPremisesDataGatewayClusterUser.CmdletVerb}-{AddOnPremisesDataGatewayClusterUser.CmdletName}",
+            typeof(AddOnPremisesDataGatewayClusterUser));
+
+        [TestMethod]
+        [TestCategory("Interactive")]
+        [TestCategory("SkipWhenLiveUnitTesting")] // Ignore for Live Unit Testing
+        public void EndToEndAddOnPremisesDataGatewayClusterUser()
+        {
+            using (var ps = System.Management.Automation.PowerShell.Create())
+            {
+                // Arrange
+                ProfileTestUtilities.ConnectToPowerBI(ps);
+                ps.AddCommand(AddOnPremisesDataGatewayClusterUserInfo)
+                    .AddParameter(nameof(AddOnPremisesDataGatewayClusterUser.GatewayClusterId), new Guid())
+                    .AddParameter(nameof(AddOnPremisesDataGatewayClusterUser.PrincipalObjectId), new Guid("{F31FEA72-8435-4871-BF75-E94168C71A6D}"))
+                    .AddParameter(nameof(AddOnPremisesDataGatewayClusterUser.AllowedDataSourceTypes), new DatasourceType[] {DatasourceType.Sql })
+                    .AddParameter(nameof(AddOnPremisesDataGatewayClusterUser.Role), "ConnectionCreator");
+
+                // Act
+                var result = ps.Invoke();
+
+                // Assert
+                TestUtilities.AssertNoCmdletErrors(ps);
+                Assert.IsNotNull(result);
+            }
+        }
+
+        [TestMethod]
+        public async Task AddOnPremisesDataGatewayClusterUserCanBeSerialized()
+        {
+            // Arrange
+            var client = Utilities.GetTestClient("");
+            var request = new GatewayClusterAddPrincipalRequest()
+            {
+                PrincipalObjectId = new Guid(),
+                AllowedDataSourceTypes = new DatasourceType[]
+                {
+                    DatasourceType.Sql
+                },
+                Role = "the role"
+
+            };
+
+            // Act
+            var result = await client.AddUsersToGatewayCluster(new Guid(), request, true);
+
+            // Assert
+            result.IsSuccessStatusCode.Should().Be(true);
+        }
+    }
+}
---
+++ src/Modules/OnPremisesDataGateway/Commands.OnPremisesDataGateway/AddOnPremisesDataGatewayClusterUser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using Microsoft.PowerBI.Common.Abstractions;
+using Microsoft.PowerBI.Common.Abstractions.Interfaces;
+using Microsoft.PowerBI.Common.Api.Gateways.Entities;
+using Microsoft.PowerBI.Common.Client;
+
+namespace Microsoft.PowerBI.Commands.OnPremisesDataGateway
+{
+    [Cmdlet(CmdletVerb, CmdletName)]
+    [OutputType(typeof(void))]
+    public class AddOnPremisesDataGatewayClusterUser : PowerBIClientCmdlet, IUserScope
+    {
+        public const string CmdletName = "OnPremisesDataGatewayClusterUser";
+        public const string CmdletVerb = VerbsCommon.Add;
+
+        [Parameter()]
+        public PowerBIUserScope Scope { get; set; } = PowerBIUserScope.Individual;
+
+        [Alias("Cluster")]
+        [Parameter()]
+        public Guid GatewayClusterId { get; set; }
+
+        [Parameter()]
+        public Guid PrincipalObjectId { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public IEnumerable<DatasourceType> AllowedDataSourceTypes { get; set; }
+
+        [Parameter()]
+        public string Role { get; set; }
+
+        public override void ExecuteCmdlet()
+        {
+            using (var client = CreateClient())
+            {
+                var request = new GatewayClusterAddPrincipalRequest
+                {
+                    PrincipalObjectId = PrincipalObjectId,
+                    AllowedDataSourceTypes = AllowedDataSourceTypes,
+                    Role = Role
+                };
+
+                var result = client.GatewaysV2.AddUsersToGatewayCluster(GatewayClusterId, request, this.Scope == PowerBIUserScope.Individual);
+            }
+        }
+    }
+}
