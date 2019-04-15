4f8e57934c0f73c4d3f6bda9adddc4c85d61daa9
Jeff Tyson <jetyson@microsoft.com>
2019-04-15 18:59:37 +0000

    RemoveOnPremisesDataGatewayClusterUser cmdlet + e2e test

--- src/Modules/OnPremisesDataGateway/Commands.OnPremisesDataGateway.Test/RemoveOnPremisesDataGatewayClusterUserTests.cs
+++ src/Modules/OnPremisesDataGateway/Commands.OnPremisesDataGateway.Test/RemoveOnPremisesDataGatewayClusterUserTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Management.Automation;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Microsoft.PowerBI.Commands.Common.Test;
+using Microsoft.PowerBI.Commands.Profile.Test;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Microsoft.PowerBI.Commands.OnPremisesDataGateway.Test
@@ -8,6 +11,31 @@ namespace Microsoft.PowerBI.Commands.OnPremisesDataGateway.Test
     [TestClass]
     public class RemoveOnPremisesDataGatewayClusterUserTests
     {
+        private static CmdletInfo RemoveOnPremisesDataGatewayClusterUserInfo { get; } = new CmdletInfo(
+            $"{RemoveOnPremisesDataGatewayClusterUser.CmdletVerb}-{RemoveOnPremisesDataGatewayClusterUser.CmdletName}",
+            typeof(RemoveOnPremisesDataGatewayClusterUser));
+
+        [TestMethod]
+        [TestCategory("Interactive")]
+        [TestCategory("SkipWhenLiveUnitTesting")] // Ignore for Live Unit Testing
+        public void EndToEndRemoveOnPremisesDataGatewayClusterUserInfo()
+        {
+            using (var ps = System.Management.Automation.PowerShell.Create())
+            {
+                // Arrange
+                ProfileTestUtilities.ConnectToPowerBI(ps);
+                ps.AddCommand(RemoveOnPremisesDataGatewayClusterUserInfo)
+                    .AddParameter(nameof(RemoveOnPremisesDataGatewayClusterUser.GatewayClusterId), new Guid())
+                    .AddParameter(nameof(RemoveOnPremisesDataGatewayClusterUser.PrincipalObjectId), new Guid("{F31FEA72-8435-4871-BF75-E94168C71A6D}"));
+
+                // Act
+                var result = ps.Invoke();
+
+                // Assert
+                TestUtilities.AssertNoCmdletErrors(ps);
+                Assert.IsNotNull(result);
+            }
+        }
         [TestMethod]
         public async Task RemoveOnPremisesDataGatewayClusterUserCanBeSerialized()
         {
---
+++ src/Modules/OnPremisesDataGateway/Commands.OnPremisesDataGateway/RemoveOnPremisesDataGatewayClusterUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Management.Automation;
+using Microsoft.PowerBI.Common.Abstractions;
+using Microsoft.PowerBI.Common.Abstractions.Interfaces;
+using Microsoft.PowerBI.Common.Client;
+
+namespace Microsoft.PowerBI.Commands.OnPremisesDataGateway
+{
+    [Cmdlet(CmdletVerb, CmdletName)]
+    [OutputType(typeof(void))]
+    public class RemoveOnPremisesDataGatewayClusterUser : PowerBIClientCmdlet, IUserScope
+    {
+        public const string CmdletName = "OnPremisesDataGatewayClusterUser";
+        public const string CmdletVerb = VerbsCommon.Remove;
+
+        [Parameter()]
+        public PowerBIUserScope Scope { get; set; } = PowerBIUserScope.Individual;
+
+        [Alias("Cluster")]
+        [Parameter()]
+        public Guid GatewayClusterId { get; set; }
+
+        [Parameter()]
+        public Guid PrincipalObjectId { get; set; }
+
+        public override void ExecuteCmdlet()
+        {
+            using (var client = CreateClient())
+            {
+                var result = client.GatewaysV2.DeleteUserOnGatewayCluster(GatewayClusterId, PrincipalObjectId, this.Scope == PowerBIUserScope.Individual).Result;
+                Logger.WriteObject(result, true);
+            }
+        }
+    }
+}
