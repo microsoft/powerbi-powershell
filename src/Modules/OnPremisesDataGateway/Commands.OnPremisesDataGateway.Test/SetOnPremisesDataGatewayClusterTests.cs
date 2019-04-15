44efa291083d7bfb4333d002e5aa83328f43ffe0
Jeff Tyson <jetyson@microsoft.com>
2019-04-15 20:09:45 +0000

    SetOnPremisesDataGatewayCluster cmdlet + e2e test

--- src/Modules/OnPremisesDataGateway/Commands.OnPremisesDataGateway.Test/SetOnPremisesDataGatewayClusterTests.cs
+++ src/Modules/OnPremisesDataGateway/Commands.OnPremisesDataGateway.Test/SetOnPremisesDataGatewayClusterTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Management.Automation;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Microsoft.PowerBI.Commands.Common.Test;
+using Microsoft.PowerBI.Commands.Profile.Test;
 using Microsoft.PowerBI.Common.Api.Gateways.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -9,6 +12,33 @@ namespace Microsoft.PowerBI.Commands.OnPremisesDataGateway.Test
     [TestClass]
     public class SetOnPremisesDataGatewayClusterTests
     {
+        private static CmdletInfo SetOnPremisesDataGatewayClusterInfo { get; } = new CmdletInfo(
+            $"{SetOnPremisesDataGatewayCluster.CmdletVerb}-{SetOnPremisesDataGatewayCluster.CmdletName}",
+            typeof(SetOnPremisesDataGatewayCluster));
+
+        [TestMethod]
+        [TestCategory("Interactive")]
+        [TestCategory("SkipWhenLiveUnitTesting")] // Ignore for Live Unit Testing
+        public void EndToEndSetOnPremisesDataGatewayCluster()
+        {
+            using (var ps = System.Management.Automation.PowerShell.Create())
+            {
+                // Arrange
+                ProfileTestUtilities.ConnectToPowerBI(ps);
+                ps.AddCommand(SetOnPremisesDataGatewayClusterInfo)
+                    .AddParameter(nameof(SetOnPremisesDataGatewayCluster.GatewayClusterId), new Guid())
+                    .AddParameter(nameof(SetOnPremisesDataGatewayCluster.Name), "name")
+                    .AddParameter(nameof(SetOnPremisesDataGatewayCluster.AllowCloudDatasourceRefresh), false);
+
+                // Act
+                var result = ps.Invoke();
+
+                // Assert
+                TestUtilities.AssertNoCmdletErrors(ps);
+                Assert.IsNotNull(result);
+            }
+        }
+
         [TestMethod]
         public async Task SetOnPremisesDataGatewayClusterCanBeSerialized()
         {
---
+++ src/Modules/OnPremisesDataGateway/Commands.OnPremisesDataGateway/SetOnPremisesDataGatewayCluster.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using Microsoft.PowerBI.Common.Abstractions;
+using Microsoft.PowerBI.Common.Abstractions.Interfaces;
+using Microsoft.PowerBI.Common.Api.Gateways.Entities;
+using Microsoft.PowerBI.Common.Client;
+
+namespace Microsoft.PowerBI.Commands.OnPremisesDataGateway
+{
+    [Cmdlet(CmdletVerb, CmdletName)]
+    [OutputType(typeof(void))]
+    public class SetOnPremisesDataGatewayCluster : PowerBIClientCmdlet, IUserScope
+    {
+        public const string CmdletName = "OnPremisesDataGatewayCluster";
+        public const string CmdletVerb = VerbsCommon.Set;
+
+        [Parameter()]
+        public PowerBIUserScope Scope { get; set; } = PowerBIUserScope.Individual;
+
+        [Alias("Cluster")]
+        [Parameter()]
+        public Guid GatewayClusterId { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public string Name { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public string Department { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public string Description { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public string ContactInformation { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public bool? AllowCloudDatasourceRefresh { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public bool? AllowCustomConnectors { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public string LoadBalancingSelectorType { get; set; }
+
+
+        [Parameter()]
+        public string Role { get; set; }
+
+        public override void ExecuteCmdlet()
+        {
+            using (var client = CreateClient())
+            {
+                var request = new PatchGatewayClusterRequest
+                {
+                    Name = Name,
+                    Department = Department,
+                    Description = Description,
+                    ContactInformation = ContactInformation,
+                    AllowCloudDatasourceRefresh = AllowCloudDatasourceRefresh,
+                    AllowCustomConnectors = AllowCustomConnectors,
+                    LoadBalancingSelectorType = LoadBalancingSelectorType
+                };
+
+                var result = client.GatewaysV2.PatchGatewayCluster(GatewayClusterId, request, this.Scope == PowerBIUserScope.Individual).Result;
+                Logger.WriteObject(result, true);
+            }
+        }
+    }
+}
