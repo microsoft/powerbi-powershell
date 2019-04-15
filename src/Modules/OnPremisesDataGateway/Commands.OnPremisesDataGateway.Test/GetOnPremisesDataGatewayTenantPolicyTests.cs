8d0c7aa1ef2fe5091e3ef1658b7f4a2af37f716a
Jeff Tyson <jetyson@microsoft.com>
2019-04-15 18:49:41 +0000

    Clean up usings

--- src/Modules/OnPremisesDataGateway/Commands.OnPremisesDataGateway.Test/GetOnPremisesDataGatewayTenantPolicyTests.cs
+++ src/Modules/OnPremisesDataGateway/Commands.OnPremisesDataGateway.Test/GetOnPremisesDataGatewayTenantPolicyTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Management.Automation;
+﻿using System.Management.Automation;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.PowerBI.Commands.Common.Test;
