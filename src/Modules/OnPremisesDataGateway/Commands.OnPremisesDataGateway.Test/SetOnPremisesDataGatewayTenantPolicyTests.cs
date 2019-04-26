e14a24bc006f29d8f6da7d1dfdf01690eb876367
Jeff Tyson <jetyson@microsoft.com>
2019-04-26 13:26:41 +0000

    SetOnPremisesDataGatewayTenantPolicy unit test

--- src/Modules/OnPremisesDataGateway/Commands.OnPremisesDataGateway.Test/SetOnPremisesDataGatewayTenantPolicyTests.cs
+++ src/Modules/OnPremisesDataGateway/Commands.OnPremisesDataGateway.Test/SetOnPremisesDataGatewayTenantPolicyTests.cs
@@ -4,9 +4,13 @@
  */
 
 using System.Management.Automation;
+using System.Net.Http;
 using Microsoft.PowerBI.Commands.Common.Test;
 using Microsoft.PowerBI.Commands.Profile.Test;
+using Microsoft.PowerBI.Common.Api;
+using Microsoft.PowerBI.Common.Api.Gateways.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace Microsoft.PowerBI.Commands.OnPremisesDataGateway.Test
 {
@@ -35,6 +39,31 @@ namespace Microsoft.PowerBI.Commands.OnPremisesDataGateway.Test
                 TestUtilities.AssertNoCmdletErrors(ps);
                 Assert.IsNotNull(result);
             }
+        }
+
+        [TestMethod]
+        public void SetOnPremisesDataGatewayTenantPolicyReturnsExpectedResults()
+        {
+            // Arrange
+            var expectedResponse = new HttpResponseMessage();
+
+            var client = new Mock<IPowerBIApiClient>();
+            client.Setup(x => x.Gateways
+                .UpdateTenantPolicy(It.IsAny<UpdateGatewayPolicyRequest>()))
+                .ReturnsAsync(expectedResponse);
+
+            var initFactory = new TestPowerBICmdletInitFactory(client.Object);
+            var cmdlet = new SetOnPremisesDataGatewayTenantPolicy(initFactory)
+            {
+                ResourceGatewayInstallPolicy = PolicyType.Restricted,
+                PersonalGatewayInstallPolicy = PolicyType.None
+            };
+
+            // Act
+            cmdlet.InvokePowerBICmdlet();
+
+            // Assert
+            TestUtilities.AssertExpectedUnitTestResults(expectedResponse, client, initFactory);
         }
     }
 }
