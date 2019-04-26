2092d0e91219f7229cdb342682f7a2223bd69764
Jeff Tyson <jetyson@microsoft.com>
2019-04-26 13:17:45 +0000

    SetOnPremisesDataGatewayCluster unit test

--- src/Modules/OnPremisesDataGateway/Commands.OnPremisesDataGateway.Test/SetOnPremisesDataGatewayClusterTests.cs
+++ src/Modules/OnPremisesDataGateway/Commands.OnPremisesDataGateway.Test/SetOnPremisesDataGatewayClusterTests.cs
@@ -5,9 +5,13 @@
 
 using System;
 using System.Management.Automation;
+using System.Net.Http;
 using Microsoft.PowerBI.Commands.Common.Test;
 using Microsoft.PowerBI.Commands.Profile.Test;
+using Microsoft.PowerBI.Common.Api;
+using Microsoft.PowerBI.Common.Api.Gateways.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace Microsoft.PowerBI.Commands.OnPremisesDataGateway.Test
 {
@@ -39,6 +43,37 @@ namespace Microsoft.PowerBI.Commands.OnPremisesDataGateway.Test
                 TestUtilities.AssertNoCmdletErrors(ps);
                 Assert.IsNotNull(result);
             }
+        }
+
+        [TestMethod]
+        public void SetOnPremisesDataGatewayClusterReturnsExpectedResults()
+        {
+            // Arrange
+            var expectedResponse = new HttpResponseMessage();
+
+            var client = new Mock<IPowerBIApiClient>();
+            client.Setup(x => x.Gateways
+                .PatchGatewayCluster(It.IsAny<Guid>(), It.IsAny<PatchGatewayClusterRequest>(), true))
+                .ReturnsAsync(expectedResponse);
+
+            var initFactory = new TestPowerBICmdletInitFactory(client.Object);
+            var cmdlet = new SetOnPremisesDataGatewayCluster(initFactory)
+            {
+                GatewayClusterId = Guid.NewGuid(),
+                Name = "name",
+                Department = "department",
+                Description = "description",
+                ContactInformation = "contactInformation",
+                AllowCloudDatasourceRefresh = true,
+                AllowCustomConnectors = true,
+                LoadBalancingSelectorType = "loadBalancingSelectorType"
+            };
+
+            // Act
+            cmdlet.InvokePowerBICmdlet();
+
+            // Assert
+            TestUtilities.AssertExpectedUnitTestResults(expectedResponse, client, initFactory);
         }
     }
 }
