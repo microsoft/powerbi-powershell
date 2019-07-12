fc138e091f6ab9409bb1878681f0f9d8df9efd23
Jeff Tyson <jetyson@microsoft.com>
2019-07-12 04:44:08 +0000

    Only write when in debug Get-OnPremisesDataGatewayClusterDatasourceStatus

--- src/Modules/OnPremisesDataGateway/Commands.OnPremisesDataGateway/GetOnPremisesDataGatewayClusterDatasourceStatus.cs
+++ src/Modules/OnPremisesDataGateway/Commands.OnPremisesDataGateway/GetOnPremisesDataGatewayClusterDatasourceStatus.cs
@@ -40,7 +40,7 @@ namespace Microsoft.PowerBI.Commands.OnPremisesDataGateway
             using (var client = CreateClient())
             {
                 var result = client.Gateways.GetGatewayClusterDatasourceStatus(GatewayClusterId, GatewayClusterDatasourceId, this.Scope == PowerBIUserScope.Individual).Result;
-                Logger.WriteObject(result, true);
+                Logger.WriteDebug(result);
             }
         }
     }
