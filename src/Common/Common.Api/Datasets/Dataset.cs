407531d98e88313407b8ae2f552479a437443ca4
Chaz Beck <chbeck@microsoft.com>
2019-08-27 14:48:56 -0700

    Fixes after merge to dev

--- src/Common/Common.Api/Datasets/Dataset.cs
+++ src/Common/Common.Api/Datasets/Dataset.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.PowerBI.Common.Api.Helpers;
+using Microsoft.PowerBI.Common.Api.Shared;
 
 namespace Microsoft.PowerBI.Common.Api.Datasets
 {
