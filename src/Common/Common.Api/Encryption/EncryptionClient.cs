e853ade6685511eda16600ee36bbbc995ebdf530
Michael Nguyen <sir.michaelmn@gmail.com>
2020-03-25 11:13:00 -0700

    Forgot encryption client files

---
+++ src/Common/Common.Api/Encryption/EncryptionClient.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.PowerBI.Api.V2;
+using Microsoft.PowerBI.Api.V2.Models;
+
+namespace Microsoft.PowerBI.Common.Api.Encryption
+{
+    public class EncryptionClient : PowerBIEntityClient, IEncryptionClient
+    {
+        public EncryptionClient(IPowerBIClient client): base(client) { }
+
+        public EncryptionKey AddPowerBIEncryptionKey(string name, string keyVaultKeyIdentifier, bool? isDefault = null, bool? activate = null)
+        {
+            var tenantKeyCreationRequest = new TenantKeyCreationRequest()
+            {
+                Name = name,
+                KeyVaultKeyIdentifier = keyVaultKeyIdentifier,
+                IsDefault = isDefault,
+                Activate = activate
+            };
+
+            return this.Client.EncryptionKeys.AddPowerBIEncryptionKey(tenantKeyCreationRequest);
+        }
+
+        public IEnumerable<EncryptionKey> GetPowerBIEncryptionKeys()
+        {
+            return this.Client.EncryptionKeys.GetPowerBIEncryptionKeys()?.Value
+                    .Select(key => (EncryptionKey)key);
+        }
+
+        public EncryptionKey RotatePowerBIEncryptionKey(Guid tenantKeyId, string keyVaultKeyIdentifier)
+        {
+            var tenantKeyRotationRequest = new TenantKeyRotationRequest()
+            {
+                KeyVaultKeyIdentifier = keyVaultKeyIdentifier
+            };
+
+            return this.Client.EncryptionKeys.RotatePowerBIEncryptionKey(tenantKeyId, tenantKeyRotationRequest);
+        }
+
+        public void SetPowerBICapacityEncryptionKey(Guid tenantKeyId, Guid capacityId)
+        {
+            var capacityPatchRequest = new CapacityPatchRequest()
+            {
+                TenantKeyId = tenantKeyId
+            };
+
+            this.Client.EncryptionKeys.PatchCapacity(capacityId, capacityPatchRequest);
+        }
+    }
+}
---
+++ src/Common/Common.Api/Encryption/IEncryptionClient.cs
@@ -0,0 +1,21 @@
+﻿/*
+* Copyright (c) Microsoft Corporation. All rights reserved.
+* Licensed under the MIT License.
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.PowerBI.Common.Api.Encryption
+{
+    public interface IEncryptionClient
+    {
+        EncryptionKey AddPowerBIEncryptionKey(string name, string keyVaultKeyIdentifier, bool? isDefault = null, bool? activate = null);
+
+        IEnumerable<EncryptionKey> GetPowerBIEncryptionKeys();
+
+        EncryptionKey RotatePowerBIEncryptionKey(Guid tenantKeyId, string keyVaultKeyIdentifier);
+
+        void SetPowerBICapacityEncryptionKey(Guid tenantKeyId, Guid capacityId);
+    }
+}
