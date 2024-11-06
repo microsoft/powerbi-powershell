1d01e171279b1c14d022b691f9e0d1c43864ce0c
Daniel Xiao <danielxiao@microsoft.com>
2024-11-05 19:44:46 -0600

    fix msal

--- src/Common/Commands.Common/PowerBICmdlet.cs
+++ src/Common/Commands.Common/PowerBICmdlet.cs
@@ -66,7 +66,7 @@ namespace Microsoft.PowerBI.Commands.Common
             string assemblyFilePath;
             if (requestedAssembly.Name == "Microsoft.Identity.Client" || requestedAssembly.Name == "Microsoft.Identity.Client.Extensions.Msal" || requestedAssembly.Name == "Microsoft.Identity.Client.Broker")
             {
-                var libType = IsNetFramework ? "net45" : "netcoreapp2.1";
+                var libType = "netstandard2.0";
                 assemblyFilePath = Path.Combine(executingDirectory, "MSAL", libType, requestedAssembly.Name + ".dll");
             }
             else
