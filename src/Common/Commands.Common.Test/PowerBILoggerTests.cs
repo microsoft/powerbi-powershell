23d35d7f4f3496f03200e6a97c4c90c759614d86
Beabhi17 <134540249+Beabhi17@users.noreply.github.com>
2025-07-04 02:24:44 -0700

    Update PowerBILoggerTests.cs

--- src/Common/Commands.Common.Test/PowerBILoggerTests.cs
+++ src/Common/Commands.Common.Test/PowerBILoggerTests.cs
@@ -47,6 +47,17 @@ namespace Microsoft.PowerBI.Commands.Common.Test
         }
 
         [TestMethod]
+        public void TestWriteWarningAddsMessage()
+        {
+            var logger = new PowerBILoggerMock();
+            string warning = "This is a warning!";
+            logger.WriteWarning(warning);
+
+            Assert.AreEqual(1, logger.WarningMessages.Count);
+            Assert.AreEqual(warning, logger.WarningMessages[0]);
+        }
+        
+        [TestMethod]
         public void TestMainThreadDetection()
         {
             int mainThread = Thread.CurrentThread.ManagedThreadId;
